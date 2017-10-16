2a32ff40d8ae0448aa7dfa3dc0f4dfe26f9db0f6
Bradley Sloan <bradsloan16@gmail.com>
2017-10-16 17:04:51 -0400

    add login/sessions and post feed/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: redirect('/posts')
+  get '/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#delete'
 end
