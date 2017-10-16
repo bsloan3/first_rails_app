9ae9367a65daead41fd5e4dd664d2fdb00b703f3
Bradley Sloan <bradsloan16@gmail.com>
2017-10-16 18:39:50 -0400

    delete and update posts, all thats left is new users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,39 @@ class PostsController < ApplicationController
   def show
     @post=Post.find_by(id: params[:id])
   end
+
+  def new
+    require_user
+    @post=Post.new
+  end
+
+  def create
+    @post=current_user.posts.new(params[:post])
+    if @post.save
+      redirect_to '/'
+    else
+      @errors = "Try again."
+      render '/posts/new'
+    end
+  end
+
+  def delete
+    @post=Post.find_by(id: params[:id])
+    @post.destroy!
+    redirect_to '/'
+  end
+
+  def edit
+    @post=Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post=Post.find_by(id: params[:id])
+    if @post.update(title: params[:post][:title], body: params[:post][:body], user_id: current_user.id)
+      redirect_to '/posts/#{@post.id}'
+    else
+      @errors = "Try again."
+      render '/posts/edit'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: redirect('/posts')
   get '/posts', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
   get '/posts/:id', to: 'posts#show'
+  post '/posts', to: 'posts#create'
+  delete '/posts/:id', to: 'posts#delete'
+  get '/posts/:id/edit', to: 'posts#edit'
+  patch '/posts/:id', to: 'posts#update'
+  # resources :posts
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
