728446a5c97e489f21fd33c3087e5336cd2fed1f
Bradley Sloan <bradsloan16@gmail.com>
2017-10-16 15:23:04 -0400

    migrate and model posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+
+user = User.create!(email: 'brad@brad.com', password: 'brad')
+
+user.posts.create!(title: 'sample title', body: 'this is a sample post')
+user.posts.create!(title: 'sample title 2', body: 'this is a second sample post')
